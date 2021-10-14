e7a96d1753446798dd1a1a01c37edfeb3baecd0a
martius <martius@DESKTOP-A2GUJGR.localdomain>
2021-10-14 18:18:17 +0200

    playbable version

--- miniGame/js/script.js
+++ miniGame/js/script.js
@@ -1,82 +1,63 @@
+   //score in game
+    let score = 0;
 
-function elo(){
-    const hitboxHead = document.createElement('div');
-    const hitboxBody = document.createElement('div');
-    const div = document.createElement('div');
-    const img = document.createElement('img');
-    const road = document.querySelector('.road');
-    let random = Math.floor(Math.random() * 4 + 2);
-    hitboxBody.classList.add('hitbox-body')
-    hitboxHead.classList.add('hitbox-head');
-    img.setAttribute("src", "images/walking.png");
-    div.appendChild(hitboxHead);
-    div.appendChild(hitboxBody);
-
-    div.classList.add('target');
-    div.appendChild(img)
-
-    div.style.animation = "run";
-    div.style.animationIterationCount = "1";
-    div.style.animationDuration = random + 's';
-    div.style.animationTimingFunction = 'linear';
+    function game(){
+        //create elements
+        const hitboxHead = document.createElement('div');
+        const hitboxBody = document.createElement('div');
+        const div = document.createElement('div');
+        const img = document.createElement('img');
+        //import elements
+        const road = document.querySelectorAll('.road');
+        //random value
+        let roadNumber = Math.floor(Math.random()*3)
+        let speed = Math.floor(Math.random() * 4 + 2);
+       
+        //add elements
+        hitboxBody.classList.add('hitbox-body')
+        hitboxHead.classList.add('hitbox-head');
 
 
-    road.appendChild(div)
+        img.setAttribute("src", "images/walking.png");
 
-    setTimeout(()=>{
-        div.remove()
-    },random * 1000)
+        div.classList.add('target');
+        div.appendChild(hitboxHead);
+        div.appendChild(hitboxBody);
+        div.appendChild(img);
 
-
-    $(hitboxBody).on('click', function() {
-        console.log('TRAFIŁES! w ciało');
-        div.remove();
-
-    })
-    $(hitboxHead).on('click', function() {
-        console.log('TRAFIŁES! w głowe');
-        div.remove();
-    })
-}
-setInterval(elo, 1500);
-
-
-
-
-/*
-let roadNumber = 0;
-
-const run = () =>{
-
-    let targetSpeed = Math.floor((Math.random() * 4) + 1);
-    let yo = targetSpeed + 's';
-    const targetS = $('.target');
-    let timee = targetSpeed * 1000;
-
-    const $targetAdd = $('<div class="target"><img src="images/walking.png"><div class="hitbox-head"></div><div class="hitbox-body"></div></div>')
-
-        targetS.css({
-            "animation": "run",
-          })
-
-        console.log(targetS.length)
-
-        if( targetS.length == 1)
-        {
-            targetS.css({
-                "animation-duration": yo
-            })
-        }
-         
-          $('.r1').append($targetAdd);
-        setTimeout(()=>{
-            $targetAdd.remove()
-        }, timee)
-}
+        //div css
         
+        $(div).css({
+            "animation" : "run",
+            "animation-iteration-count" : "1",
+            "animation-duration" : speed + "s",
+            "animation-timing-function" : "linear",
+        })
 
- 
+        //score table
+        document.querySelector('.score').textContent = score;
+        
+        //adding the target to the road
+        road[roadNumber].appendChild(div);
+
+        //auto remove target 
+        setTimeout(()=>{
+        div.remove()
+        },speed * 1000)
+
+        //click place
+        $(hitboxBody).on('click', function() {
+        div.remove();
+        score+= 100
+        })
+
+        $(hitboxHead).on('click', function() {
+        div.remove();
+        score+= 250
+
+    })
+}
+
+setInterval(game, 1500);
 
 
-setInterval(run, 500);
-*/
