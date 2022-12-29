7e9b1c66348653ca534d355d1c3d0e7ec2cc235b
Maciek <84990162+martiusx@users.noreply.github.com>
2022-12-29 23:12:51 +0100

    Delete side1 directory

--- side1/js/script-media.js
+++
@@ -1,7 +0,0 @@
-//media
-
-document.querySelector('.nav-media-button').addEventListener('click', ()=>{
-
-    document.querySelector('.nav-media').classList.toggle('active');
-    document.querySelector('.nav-media-button').classList.toggle('active');
-})
